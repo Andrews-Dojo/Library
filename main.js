d24ba5147be207a2a87b7dba22280875becf8fd9
Andrew Brown <brownand000@gmail.com>
2024-02-02 17:04:47 -0600

    added delete capabilities to createTableRow func. Deletes table row and removes that book from the library array

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function addBookToLibrary(){
     let newBook = new Book(title, author, pages, read);
     
     myLibrary.push(newBook);
-    //Display Book in table
+    //Display Book in new table row
     createTableRow();
 }
 
@@ -30,6 +30,8 @@ newBookbtn.addEventListener('click', () => {
 document.querySelector('#new-book-form').addEventListener('submit', () => {
     event.preventDefault();
     addBookToLibrary();
+    let newBookForm = document.querySelector('#new-book-form');
+    newBookForm.style.display = 'none';
 })
 
 function createTableRow() {
@@ -48,7 +50,7 @@ function createTableRow() {
     const deleteCell = document.createElement('td');
     const deleteBtn = document.createElement('button');
     deleteBtn.classList = 'deleteBtn';
-    deleteBtn.id = (i+1);
+    deleteBtn.id = (i);
 
     titleCell.innerText = book.title;
     authorCell.innerText = book.author;
@@ -67,73 +69,13 @@ function createTableRow() {
     newRow.appendChild(readCell);
     newRow.appendChild(deleteCell);
     deleteCell.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', () => {
+        table.deleteRow((deleteBtn.id));
+        myLibrary.splice((deleteBtn.id));
+    })
     }
+   
 }
 
 
-
-
-// const newBook = document.querySelector('.newBook');
-// // const formDiv = document.querySelector('.formDiv');
-// // const bookList = document.querySelector('.bookList');
-
-// // function displayBooks() {
-// //     for(book of myLibrary){
-// //        let newBook = document.createElement('li');
-// //         newBook.textContent = book;
-// //         bookList.appendChild(newBook);
-// //     }
-// // }
-
-//     // new row creation for each new book
-    
-    // const newRow = document.createElement('tr');
-    // const titleCell = document.createElement('td');
-    // const authorCell = document.createElement('td');
-    // const pagesCell = document.createElement('td');
-    // const readCell = document.createElement('td');
-    // const deleteCell = document.createElement('td');
-    // const deleteBtn = document.createElement('button');
-//     const form = document.createElement('form');
-
-// newBook.addEventListener('click', () => {
-//     const dialog = document.createElement('dialog');
-//     dialog.open = 'open';
-//     newBook.append(dialog);
-//     dialog.append(form);
-//     const author = document.createElement('input');
-//     const title = document.createElement('input');
-//     const pages = document.createElement('input');
-//     author.placeholder = 'Author';
-//     title.placeholder = 'Title';
-//     pages.placeholder = 'Number of pages';
-//     form.append(author);
-//     form.append(title);
-//     form.append(pages);
-//     const submitBtn = document.createElement("button");
-//     submitBtn.innerText = 'Submit';
-//     form.append(submitBtn);
-
-
-//     })
-// //     const dialog = document.createElement('dialog');
-// //     const dialogBtn = document.createElement('button');
-// //     dialogBtn.innerText = 'Add Book';
-// //     dialog.open = 'open';
-// //     formDiv.append(dialog)
-// //     const form = document.createElement('form');
-// //     form.id = 'form';
-// //     form.method = 'dialog';
-//     // const author = document.createElement('input');
-//     // const title = document.createElement('input');
-//     // const pages = document.createElement('input');
-//     // author.placeholder = 'Author';
-//     // title.placeholder = 'Title';
-//     // pages.placeholder = 'Number of pages';
-// //     formDiv.appendChild(dialog);
-// //     dialog.appendChild(form);
-// //     form.append(author);
-// //     form.append(title);
-// //     form.append(pages);
-// //     form.append(dialogBtn);
-// // })
